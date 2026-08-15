ab2cc5dead550e9811443ece7fb40bfa6caef330
Aarav Wagh <65918077+monalipawar@users.noreply.github.com>
2026-08-15 19:55:54 -0400

    Rename page title from 'CosmoCal' to 'TimeKeeper'

--- TK.py
+++ TK.py
@@ -7,7 +7,7 @@ import io
 from datetime import datetime, date, timedelta
 
 st.set_page_config(
-    page_title="CosmoCal",
+    page_title="TimeKeeper",
     page_icon="🌌",
     layout="wide",
     initial_sidebar_state="expanded",
